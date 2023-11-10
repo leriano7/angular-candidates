6423369e5b9a568531f9cb421548bff9211062e5
Ismael López Quintero <ismael.lopez@sic.uhu.es>
2023-11-10 17:54:16 +0100

    First test

--- src/app/services/candidates.service.spec.ts
+++ src/app/services/candidates.service.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CandidatesService } from './candidates.service';
+import { APP_CONFIG, Config } from 'src/config/app.config';
 
 describe('CandidatesService', () => {
   let service: CandidatesService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers : [{
+        provide: APP_CONFIG,
+        useValue : Config
+      }]
+    });
     service = TestBed.inject(CandidatesService);
   });
 
