8c6c60d5df6495bfc19961526e7f0112db565a42
Ismael López Quintero <ismael.lopez@sic.uhu.es>
2023-11-12 22:55:39 +0100

    Going ahead with candidate-form

--- src/app/components/candidate-form/candidate-form.component.ts
+++ src/app/components/candidate-form/candidate-form.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Candidate } from 'src/app/models/candidate';
+import { linkedinPattern, phonePattern } from 'src/app/utils/validators';
 
 @Component({
   selector: 'app-candidate-form',
@@ -7,4 +10,57 @@ import { Component } from '@angular/core';
 })
 export class CandidateFormComponent {
 
+  public candidateForm! : FormGroup;
+
+  constructor(private fb: FormBuilder){
+    this.candidateForm = this.fb.group({
+      name : ["", Validators.required, Validators.minLength(2)],
+      surname : ["", Validators.required, Validators.minLength(2)],
+      email : ["", Validators.required, Validators.email],
+      phone : ["", Validators.pattern(phonePattern)],
+      linkedIn : ["", Validators.pattern(linkedinPattern)],
+      experience : ["", Validators.required]  
+    });
+  }
+
+  @Output() candidateSubmitter = new EventEmitter<Candidate>();
+  @Output() backEmitter = new EventEmitter();
+  
+
+  public get name() {
+    return this.candidateForm.get("name");
+  }
+
+  public get surname() {
+    return this.candidateForm.get("surname");
+  }
+
+  public get email() {
+    return this.candidateForm.get("email");
+  }
+
+  public get phone() {
+    return this.candidateForm.get("phone");
+  }
+
+  public get linkedin() {
+    return this.candidateForm.get("linkedIn");
+  }
+
+  public get experience() {
+    return this.candidateForm.get("experience");
+  }
+
+  public onSubmit = () => {
+    const savedCandidate : Candidate = Object.assign({}, this.candidateForm.value);
+    this.candidateSubmitter.emit(savedCandidate);
+  };
+
+  public goBack = () => {
+    this.backEmitter.emit();
+  };
+
+  public reset = () => {
+    this.candidateForm.reset();
+  };
 }
---
+++ src/app/utils/validators.ts
@@ -0,0 +1,7 @@
+export const emailPattern: RegExp = /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/;
+
+export const phonePattern: RegExp =
+  /^(\+\d{1,3}(\s|-)?)?\d{1,14}(\s|-)?\d{1,14}$/;
+
+export const linkedinPattern: RegExp =
+  /^(?:https?:\/\/)?(?:www\.)?linkedin\.com\/in\/[^\/]+\/?$/;
