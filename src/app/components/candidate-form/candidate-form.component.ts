597cce773b595b864ad1e563d2e34a557d565b90
Ismael López Quintero <ismael.lopez@sic.uhu.es>
2023-11-13 16:19:12 +0100

    Adding candidate create form

--- src/app/components/candidate-form/candidate-form.component.ts
+++ src/app/components/candidate-form/candidate-form.component.ts
@@ -31,24 +31,69 @@ export class CandidateFormComponent {
     return this.candidateForm.get("name");
   }
 
+  public getNameErrors() {
+    if (this.name?.hasError("required")) {
+      return "Debe introducir un nombre.";
+    }
+    return this.name?.hasError("minlength")
+      ? "El nombre debe tener al menos 2 carácteres"
+      : "";
+  }
+
   public get surname() {
     return this.candidateForm.get("surname");
+  }
+
+  public getSurnameErrors() {
+    if (this.name?.hasError("required")) {
+      return "Debe introducir los apellidos.";
+    }
+    return this.name?.hasError("minlength")
+      ? "Los apellidos debe tener al menos 2 carácteres"
+      : "";
   }
 
   public get email() {
     return this.candidateForm.get("email");
   }
 
+  public getEmailErrors() {
+    if (this.email?.hasError("required")) {
+      return "Debe introducir un email.";
+    }
+    return this.email?.hasError("email")
+      ? "Debe introducir un email correcto"
+      : "";
+  }
+
   public get phone() {
     return this.candidateForm.get("phone");
+  }
+
+  public getPhoneErrors() {
+    return this.phone?.hasError("pattern")
+      ? "Debe introducir un teléfono correcto, ej (+34555667788)"
+      : "";
   }
 
   public get linkedin() {
     return this.candidateForm.get("linkedIn");
   }
 
+  public getLinkedinErrors() {
+    return this.linkedin?.hasError("pattern")
+      ? "Debe introducir un perfil de linkedin correcto"
+      : "";
+  }
+
   public get experience() {
     return this.candidateForm.get("experience");
+  }
+
+  public getExperienceErrors() {
+    return this.experience?.hasError("required")
+      ? "Debe seleccionar la experiencia"
+      : "";
   }
 
   public onSubmit = () => {
