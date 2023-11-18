d30234980ffc98a0d049f3c67e0c91e603f4ad55
Ismael López Quintero <ismael.lopez@sic.uhu.es>
2023-11-18 11:34:53 +0100

    start day 4: Adding non purity function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-101-day3';
+export class AppComponent implements OnInit {
+
+  public title = 'angular-101-day3';
+  private count = 0;
+
+  ngOnInit(): void {
+    //fromEvent(document, "click").subscribe(()=>console.log('Clicked!'));
+    document.addEventListener("click",()=>{
+      // This is not a pure function. It depends on an external variable
+      // console.log("Clicked " + this.count++);
+      console.log(`Clicked ${this.count++}`);
+    });
+  }
+
 }
