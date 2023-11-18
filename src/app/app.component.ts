06b6cd0e0d61fa9abfeff3b8b2d960f92c9abc5c
Ismael López Quintero <ismael.lopez@sic.uhu.es>
2023-11-18 11:56:49 +0100

    Adding manual throttleTime

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,27 @@ export class AppComponent implements OnInit {
   public title = 'angular-101-day3';
   //private count = 0;
 
+  // ngOnInit(): void {
+  //   // This is a pure function. It doesnt depend on an external variable.
+  //   // scan is inspired in Array.reduce -> scan all events and keeps a value.
+  //   // Takes a value and returns that value with transformation.
+  //   fromEvent(document, "click")
+  //     .pipe(scan((count) => count + 1, 0))
+  //     .subscribe((count) => console.log(`Clicked ${count}`));
+  // }
+
+  // Temp flow without observables
   ngOnInit(): void {
-    // This is a pure function. It doesnt depend on an external variable.
-    // scan is inspired in Array.reduce -> scan all events and keeps a value.
-    // Takes a value and returns that value with transformation.
-    fromEvent(document, "click")
-      .pipe(scan((count) => count + 1, 0))
-      .subscribe((count) => console.log(`Clicked ${count}`));
+    let count = 0;
+    let rate = 1000;
+    let lastClick = Date.now();
+    console.log('1 ' + lastClick);
+    document.addEventListener("click",()=>{
+      if(Date.now() - lastClick >= rate) {
+        console.log(`Clicked ${count++}`);
+        lastClick = Date.now();
+        console.log('2 ' + lastClick);
+      }
+    });
   }
 }
