48d1c83f297127f882c20e4cf42fa48098fca4e8
Ismael López Quintero <ismael.lopez@sic.uhu.es>
2023-11-24 19:29:51 +0100

    Commenting

--- src/app/authentication.guard.ts
+++ src/app/authentication.guard.ts
@@ -5,9 +5,6 @@ import { UserService } from './services/user.service';
 export const authenticationGuard: CanActivateFn = () => {
   const router = inject(Router);
   const userService = inject(UserService);
-  // Coherción a booleano.
-  // Esto quiere decir que si es distinto de nulo, será verdadero.
-  // Si es nulo, será falso.
 
   if(userService.isLogged()) return true;
   return router.parseUrl('/login');
