08125fd8bcce432a3b6e171483f550067094fcb7
Ismael López Quintero <ismael.lopez@sic.uhu.es>
2023-11-26 00:31:43 +0100

    Fixing tests

--- src/app/components/project-card/project-card.component.spec.ts
+++ src/app/components/project-card/project-card.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProjectCardComponent } from './project-card.component';
+import { AppMaterialModule } from 'src/app/app-material.module';
 
 describe('ProjectCardComponent', () => {
   let component: ProjectCardComponent;
@@ -8,10 +9,17 @@ describe('ProjectCardComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports : [AppMaterialModule],
       declarations: [ProjectCardComponent]
     });
     fixture = TestBed.createComponent(ProjectCardComponent);
     component = fixture.componentInstance;
+    component.project = {
+      name : 'Angulando',
+      description : 'Un proyecto en angular',
+      technology : ['Angular','CSS'],
+      experience : 2
+    };
     fixture.detectChanges();
   });
 
